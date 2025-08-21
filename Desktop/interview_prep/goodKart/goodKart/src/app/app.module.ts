5ca5c09d486cf1b59e56b78aeb4e65e1b4b3aba7
Shanvi Agrawal <shanvikansal3.sa@gmail.com>
2025-08-21 23:40:53 +0530

    search

--- Desktop/interview_prep/goodKart/goodKart/src/app/app.module.ts
+++ Desktop/interview_prep/goodKart/goodKart/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SearchComponent } from './product-list/search/search.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainMenuComponent,
+    TopMenuComponent,
+    ProductListComponent,
+    SearchComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
